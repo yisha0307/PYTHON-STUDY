71e078757d05713c7975af46e2a4968708f5c278
chenyisha <chenchen9037@126.com>
2019-12-03 16:21:13 +0800

    learn django

---
+++ OA/hrs/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+
+depts_list = [
+    {'no': 15, 'name': '财务部', 'location': '北京'},
+    {'no': 20, 'name': '研发部', 'location': '成都'},
+    {'no': 30, 'name': '销售部', 'location': '上海'},
+]
+
+def index(request):
+    return render(request, 'index.html', {'depts_list': depts_list})
